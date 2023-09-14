2db46ca6b7d6de20be6acf1a94290c88d8a96fad
sunnat <123@.ru>
2023-09-14 22:07:53 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var answer5 = prompt('Ведите имя');
+var answer0 = prompt('Ведите Свой возраст');     
+var answer = prompt('Решите пример 6 + 3 = ?');     
+var answer1 = prompt('Решите пример 6 - 3 = ?');   
+var answer2 = prompt('Решите пример 6 * 3 = ?'); 
+var answer3 = prompt('Решите пример 6 / 3 = ?'); 
+var answer4 = prompt('Решите пример 6 % 3 = ?'); 
+var end = ('Open console')
+
+var myInfo1 = 'Меня ' + ' зовут ' + answer5;
+var myInfo2 = 'Мне ' + answer0 + ' лет';
+var myInfo3 = 'Ваш ' + ' ответ ' + ' = ' + answer;
+var myInfo4 = 'Ваш ' + ' ответ ' + ' = ' + answer1;
+var myInfo5 = 'Ваш ' + ' ответ ' + ' = ' + answer2;
+var myInfo6 = 'Ваш ' + ' ответ ' + ' = ' + answer3;
+var myInfo7 = 'Ваш ' + ' ответ ' + ' = ' + answer4;
+console.log(myInfo1);
+console.log(myInfo2);
+console.log(myInfo3);
+console.log(myInfo4);
+console.log(myInfo5);
+console.log(myInfo6);
+console.log(myInfo7);
